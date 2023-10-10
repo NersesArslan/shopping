2be8dd41eadc3d651207456d17f23eb11ebeebe7
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2023-10-09 21:01:39 -0700

    fetched image and title, but looks messy.

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,16 +2,35 @@ import { useState, useEffect } from "react"
 
 
 export default function Product ({key}) {
+    const [imageURL, setImageURL] = useState(null)
+    const [title, setTitle] = useState(null)
+    const [price, setPrice] = useState(null)
     useEffect(() => {
-        fetch('https://fakestoreapi.com/products/1')
+        fetch('https://fakestoreapi.com/products/1', { mode: "cors" })
             .then(res=>res.json())
-            .then(json=>console.log(json))
+            .then(json=> setImageURL(json.image))
+            .catch((error) => console.error(error));
+        fetch('https://fakestoreapi.com/products/1', { mode: "cors" })
+            .then(res=>res.json())
+            .then(json=> setTitle(json.title))
+            .catch((error) => console.error(error));
+        fetch('https://fakestoreapi.com/products/1', { mode: "cors" })
+            .then(res=>res.json())
+            .then(json=> setPrice(json.price))
+            .catch((error) => console.error(error));
     }, [])
     return(
-        <div key={key}>
-            <div className="card"></div>
-            <img src={json.image} alt="" />
-            <h4>Product Title</h4>
-        </div>
+       
+        imageURL && (
+           
+            
+            <div className="card">
+                <img src={imageURL} />
+                <h3>{title}</h3>
+                <p>Price: ${price}</p>
+            </div>
+         
+           )
+       
     )
 }
